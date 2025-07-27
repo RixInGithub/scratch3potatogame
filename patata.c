c983e9320dbe97c034545176521863932d665d9b
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2025-07-27 20:18:15 +0300

    Update patata.c

--- patata.c
+++ patata.c
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 #include <stdlib.h>
 #define TAB "\x20\x20\x20\x20"
@@ -23,3 +24,38 @@ int main() {
 	free(sep);
 	return 0;
 }
+*/
+
+// You can run a larger C program (FizzBuzz) by changing this condition.
+// Note you may not want to do this for slower backends (e.g., Brainfuck).
+#if 1
+int putchar(int c);
+
+int main() {
+  const char* p = "Hello, world!\n";
+  for (; *p; p++)
+    putchar(*p);
+  return 0;
+}
+
+#else
+
+#include <stdio.h>
+
+int main() {
+  for (int i = 1; i <= 100; i++) {
+    if (i % 5) {
+      if (i % 3) {
+        printf("%d\n", i);
+      } else {
+        printf("Fizz\n");
+      }
+    } else {
+      printf("FizzBuzz\n" + i * i % 3 * 4);
+    }
+  }
+  return 0;
+}
+
+#endif
+
