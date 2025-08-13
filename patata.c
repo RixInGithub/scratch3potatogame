bdc2fbd6aff56e374c3da05d298ed6ba27bbdd23
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2025-08-13 20:13:11 +0300

    reals

--- patata.c
+++ patata.c
@@ -5,12 +5,12 @@
 
 int main() {
 	printf("welkommen to patata game!");
-	printf("there are a few things before you need to know start playinh:");
+	printf("there are a few things before you need to know before u start playinh:");
 	printf(TAB "1. this game is made in c, and compiled to scratch using elvm.");
 	printf(TAB "2. this whole project might seem a bit too slow if you're on a lower-end-ish system");
 	printf(TAB "3. the game's c code is on https://github.com/RixInGithub/scratch3potatogame/");
-	printf(TAB "4. a thing before you  need to knowstart playinh");
-	printf(TAB "6. i skipped 5 bcuz the number is overrated (idek man)");
+	printf(TAB "4. a thing before you need to know before u start playinh");
+	printf(TAB "6. i skipped 5 bcuz the number is overrated (too much 5 glazers)");
 	printf("");
 	printf("input anything to begin the game.");
 	(void)getchar();
