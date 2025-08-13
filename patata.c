c62dd1d4a46e741e601c397909f3c7afdde86180
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2025-08-13 21:54:12 +0300

    update

--- patata.c
+++ patata.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define TAB "\x20\x20\x20\x20"
-#define SEPSIZ 16 // that macro name is one "T" away from smth not funny...
+#define SEPSIZ 120 // that macro name is one "T" away from smth not funny...
 
 int main() {
-	printf("welkommen to patata game!\n");
-	printf("there are a few things before you need to know before u start playinh:\n");
-	printf(TAB "1. this game is made in c, and compiled to scratch using elvm.\n");
-	printf(TAB "2. this whole project might seem a bit too slow if you're on a lower-end-ish system\n");
+	printf("welkommen to the patata game!\n");
+	printf("there are a few things you need to know before u start playinh:\n");
+	printf(TAB "1. this game is made in c, and compiled to scratch using a powerful toolchain called elvm.\n");
+	printf(TAB "2. this whole project might become a bit too slow if you're on a more lower-end system\n");
 	printf(TAB "3. the game's c code is on https://github.com/RixInGithub/scratch3potatogame/\n");
-	printf(TAB "4. a thing before you need to know before u start playinh\n");
+	printf(TAB "4. a thing you need to know before u start playinh\n");
 	printf(TAB "6. i skipped 5 bcuz the number is overrated (too much 5 glazers)\n");
+	printf(TAB "7. to input unicode characters use \"＼\" instead of \"\\\"\n"); // the \"＼\" part looks trippy
+	printf(TAB "8. you will play as a potato who wants to kill every other vegatable in the world\n");
 	printf("\n");
-	printf("input anything to begin the game.\n");
+	printf("input anything to begin the game.");
 	(void)getchar();
 	printf("\n");
-	// this is just be bein silly and testing out the capabilities
+	// this is just me bein silly and testing out the capabilities of c on scratch
 	char*sep=malloc(sizeof(char)*(SEPSIZ+1));
-	int count = 0;
-	while(count<SEPSIZ)sep[count++]="-"[0];
+	memset(sep,"-"[0],SEPSIZ);
+	sep[SEPSIZ+1]=0;
 	printf("%s",sep);
 	free(sep);
 	return 0;
