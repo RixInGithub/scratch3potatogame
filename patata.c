b4f465c3ac3651c9efe253d117d547a8125ea60b
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2025-08-14 17:51:38 +0300

    Update patata.c

--- patata.c
+++ patata.c
@@ -11,7 +11,7 @@ int main() {
 	printf(TAB "2. this whole project might become a bit too slow if you're on a more lower-end system\n");
 	printf(TAB "3. the game's c code is on https://github.com/RixInGithub/scratch3potatogame/\n");
 	printf(TAB "4. a thing you need to know before u start playinh\n");
-	printf(TAB "6. i skipped 5 bcuz the number is overrated (too much 5 glazers)\n");
+	printf(TAB "6. i skipped 5 bcuz the number is overrated (too much 5 enjoyers)\n");
 	printf(TAB "7. to input unicode characters use the unicode character in the instructions section instead of \"\\\"\n"); // "＼"
 	printf(TAB "8. all the instructions are within the game\n");
 	printf("\n");
@@ -20,23 +20,23 @@ int main() {
 	// this is just me bein silly and testing out the capabilities of c on scratch
 	char*sep=malloc(sizeof(char)*(SEPSIZ+1));
 	memset(sep,"-"[0],SEPSIZ);
-	sep[SEPSIZ+1]=0;
+	sep[SEPSIZ]=0;
 	printf("\n%s\n",sep);
-	printf("please give yourself a name before we continue:");
-	char*name=malloc(sizeof(char));
-	name[0]=0;
+	printf("please give yourself a name before we continue (8 letters+spaces only, do NOT overinput please):");
+	char*name=malloc(sizeof(char)*9);
+	name[4]=0;
+	int nLen=0;
 	char c;
-	while ((c=getchar())!=10) {
-		printf("%d\n",c);
-		int len = strlen(name)+2;
-		char*tmp=malloc(sizeof(char)*len); // i will not believe that sizeof(char)==1, especially in funky diy compiler land
-		strcpy(tmp,name);
-		tmp[len-2]=c;
-		tmp[len-1]=0;
-		free(name);
-		name=tmp;
+	while ((c=getchar())&&(nLen<8)) {
+		if(!((c!=32)||(((c>=65)||(c<=90))||((c>=97)||(c<=122)))))continue;
+		name[nLen]=c;
+		// if(name[nLen+1]==32)continue;
+		// 32>90 is false therefore the char will NOT turn into a null char.
+		if(name[nLen]>90)name[nLen]-=32;
+		nLen++;
 	}
-	printf("\n\nawesome choice, my dear %s! lets start.", name);
+	
+	printf("\nawesome choice, my dear %s! let's start...", name);
 	printf("\n%s\n",sep);
 	free(sep);
 	return 0;
